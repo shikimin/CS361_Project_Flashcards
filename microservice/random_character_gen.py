c5d3ae298b414f5616cfefb5c17bdf3d810890d7
Minyoung Shin <mshin794@gmail.com>
2023-11-19 03:35:15 -0800

    comments

--- microservice/random_character_gen.py
+++ microservice/random_character_gen.py
@@ -3,10 +3,12 @@ import time
 import random
 
 while True:
+    # Read txt file every 5 seconds
     time.sleep(5)
     with open('random_character_gen.txt') as read_file:
         data = read_file.read()
 
+    # "run" has been written in txt file
     if data == "run":
         query = '''
             query ($page: Int, $perPage: Int) {
@@ -43,15 +45,18 @@ while True:
 
         url = 'https://graphql.anilist.co'
 
-        # Make request to API
+        # Make request to API using query
         response = requests.post(url, json={'query': query, 'variables': variables}).json()
+        
+        # Parse character ID numbers from json
         response = response['data']['Page']['characters']
 
-        # get list of character ID numbers
+        # Create array of character ID numbers
         id_numbers = []
         for character in response:
             id_numbers.append(character['id'])
 
+        # Choose random ID number from array
         random_id = random.choice(id_numbers)
 
         with open('random_character_gen.txt','w', encoding="utf-8") as write_file:  
